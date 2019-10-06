9098c670db4576c4e902e313e52505b3aaea4406
riko-harada <55473651+riko-harada@users.noreply.github.com>
2019-10-06 21:20:25 +0900

    Update env.php

--- env.php
+++ env.php
@@ -1,8 +1,8 @@
 <?php
   // 環境設定
-  define('CLIENT_ID', '228N4H'); // OAuth 2.0 Client ID
-  define('CLIENT_SECRET', 'afb2997a0d0e957274c9a023a19da9de'); // Client Secret
-  define('CALLBACK_URL', 'http://localhost:9000/callback.php'); // Callback URL
+  define('CLIENT_ID', '22B4GW'); // OAuth 2.0 Client ID
+  define('CLIENT_SECRET', '23a9878361af40c92f6050f8beac8f97'); // Client Secret
+  define('CALLBACK_URL', 'http://localhost:80/callback.php'); // Callback URL
   define('AUTH_URL', 'https://www.fitbit.com/oauth2/authorize'); // OAuth 2.0: Authorization URI
   define('TOKEN_URL', 'https://api.fitbit.com/oauth2/token'); // OAuth 2.0: Access/Refresh Token Request URI
 ?>
